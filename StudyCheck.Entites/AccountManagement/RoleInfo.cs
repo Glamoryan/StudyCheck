0d25b6f44f91f0446bc3d8baaadb994b1906c23b
Adnan Özdemir <xsabrixsabrix@hotmail.com>
2020-02-19 23:50:06 +0300

    RoleInfo Enumu eklendi

---
+++ StudyCheck.Entites/AccountManagement/RoleInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyCheck.Entites.AccountManagement
+{
+    public static class RoleInfo
+    {
+        public static int Id { get; set; }
+        public static string RolAdi { get; set; }
+        public static DateTime RolKayitTarihi { get; set; }
+        public static int RoluEkleyenId { get; set; }
+        public static DateTime RolGuncellemeTarihi { get; set; }
+        public static int SilId { get; set; }
+        public static int YetkiId { get; set; }
+
+    }
+}
