ea9db846ff832ba1c877fa69c4570e6232ddacfb
Adnan Özdemir <xsabrixsabrix@hotmail.com>
2020-02-19 23:29:43 +0300

    AccountManagement kalsörü oluşturuldu ,LoginInfo-> AccountManagement kalsörüne taşındı

--- StudyCheck.Entites/AccountManagement/LoginInfo.cs
+++ StudyCheck.Entites/AccountManagement/LoginInfo.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StudyCheck.Entites.ComplexTypes
+namespace StudyCheck.Entites.AccountManagement
 {
     public static class LoginInfo
     {
