78e2a5d597ee0ceaf2c12b6ea4f7b8781ff97977
Adnan Özdemir <xsabrixsabrix@hotmail.com>
2020-02-22 18:49:23 +0300

    Kullanıcı widgetı dashboarda eklendi

--- StudyCheck.FormsUI/AdminForms/UserControls/DashboardControl.Designer.cs
+++ StudyCheck.FormsUI/AdminForms/UserControls/DashboardControl.Designer.cs
@@ -28,13 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.widgetsControl1 = new StudyCheck.FormsUI.AdminForms.UserControls.WidgetsControl();
             this.SuspendLayout();
+            // 
+            // widgetsControl1
+            // 
+            this.widgetsControl1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(83)))), ((int)(((byte)(17)))));
+            this.widgetsControl1.Location = new System.Drawing.Point(26, 22);
+            this.widgetsControl1.Name = "widgetsControl1";
+            this.widgetsControl1.Size = new System.Drawing.Size(185, 155);
+            this.widgetsControl1.TabIndex = 0;
             // 
             // DashboardControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(70)))), ((int)(((byte)(93)))));
+            this.Controls.Add(this.widgetsControl1);
             this.Name = "DashboardControl";
             this.Size = new System.Drawing.Size(1045, 649);
             this.ResumeLayout(false);
@@ -42,5 +52,7 @@
         }
 
         #endregion
+
+        private WidgetsControl widgetsControl1;
     }
 }
