e4aeddacd45d9241eeb27a44a6eb834dfd809069
Adnan Özdemir <xsabrixsabrix@hotmail.com>
2020-02-25 23:24:23 +0300

    Dashboarda title eklendi

--- StudyCheck.FormsUI/AdminForms/UserControls/DashboardControl.Designer.cs
+++ StudyCheck.FormsUI/AdminForms/UserControls/DashboardControl.Designer.cs
@@ -33,12 +33,14 @@
             this.accountWidget = new StudyCheck.FormsUI.AdminForms.UserControls.WidgetsControl();
             this.userWidget = new StudyCheck.FormsUI.AdminForms.UserControls.WidgetsControl();
             this.lessonWidget = new StudyCheck.FormsUI.AdminForms.UserControls.WidgetsControl();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // examWidget
             // 
             this.examWidget.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(83)))), ((int)(((byte)(17)))));
-            this.examWidget.Location = new System.Drawing.Point(839, 22);
+            this.examWidget.Location = new System.Drawing.Point(839, 102);
             this.examWidget.Name = "examWidget";
             this.examWidget.Size = new System.Drawing.Size(185, 155);
             this.examWidget.TabIndex = 3;
@@ -46,7 +48,7 @@
             // adminWidget
             // 
             this.adminWidget.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(83)))), ((int)(((byte)(17)))));
-            this.adminWidget.Location = new System.Drawing.Point(565, 22);
+            this.adminWidget.Location = new System.Drawing.Point(565, 102);
             this.adminWidget.Name = "adminWidget";
             this.adminWidget.Size = new System.Drawing.Size(185, 155);
             this.adminWidget.TabIndex = 2;
@@ -54,7 +56,7 @@
             // accountWidget
             // 
             this.accountWidget.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(83)))), ((int)(((byte)(17)))));
-            this.accountWidget.Location = new System.Drawing.Point(293, 22);
+            this.accountWidget.Location = new System.Drawing.Point(293, 102);
             this.accountWidget.Name = "accountWidget";
             this.accountWidget.Size = new System.Drawing.Size(185, 155);
             this.accountWidget.TabIndex = 1;
@@ -62,7 +64,7 @@
             // userWidget
             // 
             this.userWidget.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(83)))), ((int)(((byte)(17)))));
-            this.userWidget.Location = new System.Drawing.Point(20, 22);
+            this.userWidget.Location = new System.Drawing.Point(20, 102);
             this.userWidget.Name = "userWidget";
             this.userWidget.Size = new System.Drawing.Size(185, 155);
             this.userWidget.TabIndex = 0;
@@ -70,16 +72,37 @@
             // lessonWidget
             // 
             this.lessonWidget.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(83)))), ((int)(((byte)(17)))));
-            this.lessonWidget.Location = new System.Drawing.Point(20, 241);
+            this.lessonWidget.Location = new System.Drawing.Point(20, 321);
             this.lessonWidget.Name = "lessonWidget";
             this.lessonWidget.Size = new System.Drawing.Size(185, 155);
             this.lessonWidget.TabIndex = 4;
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblWelcome.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(83)))), ((int)(((byte)(17)))));
+            this.lblWelcome.Location = new System.Drawing.Point(27, 27);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(86, 21);
+            this.lblWelcome.TabIndex = 5;
+            this.lblWelcome.Text = "Dashboard";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(83)))), ((int)(((byte)(17)))));
+            this.panel1.Location = new System.Drawing.Point(20, 51);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1025, 2);
+            this.panel1.TabIndex = 6;
             // 
             // DashboardControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(70)))), ((int)(((byte)(93)))));
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblWelcome);
             this.Controls.Add(this.lessonWidget);
             this.Controls.Add(this.examWidget);
             this.Controls.Add(this.adminWidget);
@@ -89,6 +112,7 @@
             this.Size = new System.Drawing.Size(1045, 649);
             this.Load += new System.EventHandler(this.DashboardControl_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -99,5 +123,7 @@
         private WidgetsControl adminWidget;
         private WidgetsControl examWidget;
         private WidgetsControl lessonWidget;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.Panel panel1;
     }
 }
