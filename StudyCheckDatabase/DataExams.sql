d9441aff04200664623e5aab55d8169e8597617f
Adnan Özdemir <52251522+Glamoryan@users.noreply.github.com>
2020-03-05 12:03:10 +0300

    Data Insert Guncellendi

--- StudyCheckDatabase/DataExams.sql
+++ StudyCheckDatabase/DataExams.sql
@@ -2,8 +2,8 @@ use StudyCheck
 go
 insert into Uyeler (uye_ad,uye_soyad) values ('steve','jobs')
 go 
-insert into UyeDetay (uye_id,kullanici_adi,kullanici_sifre,kullanici_mail,kayit_tarihi,guncelleme_tarihi,sil_id,tema_id,rol_id)
-values (1,'admin','admin123','steve@mail',GETDATE(),GETDATE(),1,1,1)
+insert into UyeDetay (uye_id,kullanici_adi,kullanici_sifre,kullanici_mail,kayit_tarihi,guncelleme_tarihi,sil_id)
+values (1,'admin','admin123','steve@mail',GETDATE(),GETDATE(),1)
 go
 insert into Sinavlar (sinav_ad,sinav_tarih,eklenme_tarihi,ekleyen_id,guncelleme_tarihi,sil_id)
 values ('dgs',GETDATE(),GETDATE(),1,GETDATE(),1)
@@ -16,3 +16,5 @@ insert into Roller (rol_adi,rol_kayit_tarihi,ekleyen_id,rol_guncelleme_tarihi,si
 values ('admin',GETDATE(),1,GETDATE(),1,1)
 go
 insert into Temalar (tema_adi,eklenme_tarihi,ekleyen_id,sil_id) values ('default',GETDATE(),1,1)
+go
+update UyeDetay set rol_id = 1, tema_id=1 where id = 1
