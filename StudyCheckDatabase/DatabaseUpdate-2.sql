c5fe34183dd8a885f709908bf62726ae04c6ce34
Adnan Özdemir <xsabrixsabrix@hotmail.com>
2020-03-08 16:20:41 +0300

    DatabaseUpdate-2 yayınlandı

---
+++ StudyCheckDatabase/DatabaseUpdate-2.sql
@@ -0,0 +1,19 @@
+use StudyCheck
+go
+alter table UyeDetay
+add guncelleyen_id int
+go
+alter table Dersler
+add guncelleyen_id int
+go
+alter table Roller
+add guncelleyen_id int
+go
+alter table Sinavlar
+add guncelleyen_id int
+go
+alter table Yetkiler
+add guncelleyen_id int
+go
+alter table Yetkiler
+add guncelleme_tarihi Datetime2
