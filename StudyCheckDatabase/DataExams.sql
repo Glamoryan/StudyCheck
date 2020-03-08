6096e09c2d24c5a2bd5a24fed0125bbf8eac5291
Adnan Özdemir <xsabrixsabrix@hotmail.com>
2020-03-08 19:36:34 +0300

    DataInsert guncellendi

--- StudyCheckDatabase/DataExams.sql
+++ StudyCheckDatabase/DataExams.sql
@@ -8,12 +8,12 @@ go
 insert into Sinavlar (sinav_ad,sinav_tarih,eklenme_tarihi,ekleyen_id,guncelleme_tarihi,sil_id)
 values ('dgs',GETDATE(),GETDATE(),1,GETDATE(),1)
 go
-insert into Yetkiler (yetki_adi,ekleyen_id,eklenme_tarihi,sil_id) values ('Full',1,GETDATE(),1)
+insert into Yetkiler (yetki_adi,ekleyen_id,eklenme_tarihi,sil_id) values ('User',1,GETDATE(),1),('Full',1,GETDATE(),1)
 go
 insert into Dersler (sinav_id,ders_ad,eklenme_tarihi,ekleyen_id,guncelleme_tarihi,sil_id) values (1,'matematik',GETDATE(),1,GETDATE(),1)
 go
 insert into Roller (rol_adi,rol_kayit_tarihi,ekleyen_id,rol_guncelleme_tarihi,sil_id,yetki_id) 
-values ('admin',GETDATE(),1,GETDATE(),1,1)
+values ('user',GETDATE(),1,GETDATE(),1,1) , ('admin',GETDATE(),1,GETDATE(),1,2)
 go
 insert into Temalar (tema_adi,eklenme_tarihi,ekleyen_id,sil_id) values ('default',GETDATE(),1,1)
 go
