a644020376411d18095f4c88b2e00ee99f6ab870
Adnan Özdemir <xsabrixsabrix@hotmail.com>
2020-03-10 20:07:10 +0300

    LessonSettingsInfos Eklendi

---
+++ StudyCheck.FormsUI/Statikler/LessonSettingsInfos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyCheck.FormsUI.Statikler
+{
+    public static class LessonSettingsInfos
+    {
+        public static string dersAdi { get; set; }
+        public static int silId { get; set; }
+        public static int sinavId { get; set; }
+
+    }
+}
