e7d297432fe875147a26ac5e07b5ef85db33d933
Adnan Özdemir <xsabrixsabrix@hotmail.com>
2020-03-11 19:41:25 +0300

    ExamRowsControl eklendi

---
+++ StudyCheck.FormsUI/AdminForms/UserControls/ExamControl/ExamRowsControl.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace StudyCheck.FormsUI.AdminForms.UserControls.ExamControl
+{
+    partial class ExamRowsControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDuzenle = new System.Windows.Forms.Button();
+            this.lblSinavAd = new System.Windows.Forms.Label();
+            this.lblSinavId = new System.Windows.Forms.Label();
+            this.lblSinavTarihi = new System.Windows.Forms.Label();
+            this.lblKayitTarihi = new System.Windows.Forms.Label();
+            this.lblEkleyen = new System.Windows.Forms.Label();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDuzenle
+            // 
+            this.btnDuzenle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(206)))), ((int)(((byte)(109)))));
+            this.btnDuzenle.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDuzenle.FlatAppearance.BorderSize = 0;
+            this.btnDuzenle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDuzenle.Image = global::StudyCheck.FormsUI.Properties.Resources.edit_16px;
+            this.btnDuzenle.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnDuzenle.Location = new System.Drawing.Point(956, 10);
+            this.btnDuzenle.Name = "btnDuzenle";
+            this.btnDuzenle.Size = new System.Drawing.Size(40, 23);
+            this.btnDuzenle.TabIndex = 6;
+            this.btnDuzenle.UseVisualStyleBackColor = false;
+            // 
+            // lblSinavAd
+            // 
+            this.lblSinavAd.AutoSize = true;
+            this.lblSinavAd.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSinavAd.ForeColor = System.Drawing.Color.White;
+            this.lblSinavAd.Location = new System.Drawing.Point(158, 12);
+            this.lblSinavAd.Name = "lblSinavAd";
+            this.lblSinavAd.Size = new System.Drawing.Size(59, 17);
+            this.lblSinavAd.TabIndex = 9;
+            this.lblSinavAd.Text = "SınavAd";
+            // 
+            // lblSinavId
+            // 
+            this.lblSinavId.AutoSize = true;
+            this.lblSinavId.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSinavId.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(83)))), ((int)(((byte)(17)))));
+            this.lblSinavId.Location = new System.Drawing.Point(15, 12);
+            this.lblSinavId.Name = "lblSinavId";
+            this.lblSinavId.Size = new System.Drawing.Size(55, 17);
+            this.lblSinavId.TabIndex = 8;
+            this.lblSinavId.Text = "SınavId";
+            // 
+            // lblSinavTarihi
+            // 
+            this.lblSinavTarihi.AutoSize = true;
+            this.lblSinavTarihi.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSinavTarihi.ForeColor = System.Drawing.Color.White;
+            this.lblSinavTarihi.Location = new System.Drawing.Point(308, 12);
+            this.lblSinavTarihi.Name = "lblSinavTarihi";
+            this.lblSinavTarihi.Size = new System.Drawing.Size(79, 17);
+            this.lblSinavTarihi.TabIndex = 9;
+            this.lblSinavTarihi.Text = "SınavTarihi";
+            // 
+            // lblKayitTarihi
+            // 
+            this.lblKayitTarihi.AutoSize = true;
+            this.lblKayitTarihi.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKayitTarihi.ForeColor = System.Drawing.Color.White;
+            this.lblKayitTarihi.Location = new System.Drawing.Point(460, 12);
+            this.lblKayitTarihi.Name = "lblKayitTarihi";
+            this.lblKayitTarihi.Size = new System.Drawing.Size(78, 17);
+            this.lblKayitTarihi.TabIndex = 9;
+            this.lblKayitTarihi.Text = "KayıtTarihi";
+            // 
+            // lblEkleyen
+            // 
+            this.lblEkleyen.AutoSize = true;
+            this.lblEkleyen.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblEkleyen.ForeColor = System.Drawing.Color.White;
+            this.lblEkleyen.Location = new System.Drawing.Point(645, 12);
+            this.lblEkleyen.Name = "lblEkleyen";
+            this.lblEkleyen.Size = new System.Drawing.Size(59, 17);
+            this.lblEkleyen.TabIndex = 9;
+            this.lblEkleyen.Text = "Ekleyen";
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDurum.ForeColor = System.Drawing.Color.White;
+            this.lblDurum.Location = new System.Drawing.Point(824, 12);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(52, 17);
+            this.lblDurum.TabIndex = 9;
+            this.lblDurum.Text = "Durum";
+            // 
+            // ExamRowsControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(45)))), ((int)(((byte)(59)))));
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.lblEkleyen);
+            this.Controls.Add(this.lblKayitTarihi);
+            this.Controls.Add(this.lblSinavTarihi);
+            this.Controls.Add(this.lblSinavAd);
+            this.Controls.Add(this.lblSinavId);
+            this.Controls.Add(this.btnDuzenle);
+            this.Name = "ExamRowsControl";
+            this.Size = new System.Drawing.Size(1032, 39);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button btnDuzenle;
+        public System.Windows.Forms.Label lblSinavAd;
+        public System.Windows.Forms.Label lblSinavId;
+        public System.Windows.Forms.Label lblSinavTarihi;
+        public System.Windows.Forms.Label lblKayitTarihi;
+        public System.Windows.Forms.Label lblEkleyen;
+        public System.Windows.Forms.Label lblDurum;
+    }
+}
