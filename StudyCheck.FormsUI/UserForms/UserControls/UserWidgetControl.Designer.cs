47a47bcdcdbe4ef3aec5a0b6af20af6e24ac792f
Adnan Özdemir <xsabrixsabrix@hotmail.com>
2020-03-21 20:28:50 +0300

    UserWidgetControl eklendi

---
+++ StudyCheck.FormsUI/UserForms/UserControls/UserWidgetControl.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace StudyCheck.FormsUI.UserForms.UserControls
+{
+    partial class UserWidgetControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSonCalismaTarihi = new System.Windows.Forms.Label();
+            this.lblToplamCalisilanSaat = new System.Windows.Forms.Label();
+            this.lblUyeSoyad = new System.Windows.Forms.Label();
+            this.lblUyeAd = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblSonCalismaTarihi
+            // 
+            this.lblSonCalismaTarihi.AutoSize = true;
+            this.lblSonCalismaTarihi.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonCalismaTarihi.ForeColor = System.Drawing.Color.White;
+            this.lblSonCalismaTarihi.Location = new System.Drawing.Point(160, 140);
+            this.lblSonCalismaTarihi.Name = "lblSonCalismaTarihi";
+            this.lblSonCalismaTarihi.Size = new System.Drawing.Size(78, 17);
+            this.lblSonCalismaTarihi.TabIndex = 4;
+            this.lblSonCalismaTarihi.Text = "21.03.2020";
+            // 
+            // lblToplamCalisilanSaat
+            // 
+            this.lblToplamCalisilanSaat.AutoSize = true;
+            this.lblToplamCalisilanSaat.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblToplamCalisilanSaat.ForeColor = System.Drawing.Color.White;
+            this.lblToplamCalisilanSaat.Location = new System.Drawing.Point(177, 102);
+            this.lblToplamCalisilanSaat.Name = "lblToplamCalisilanSaat";
+            this.lblToplamCalisilanSaat.Size = new System.Drawing.Size(35, 17);
+            this.lblToplamCalisilanSaat.TabIndex = 5;
+            this.lblToplamCalisilanSaat.Text = "244 ";
+            // 
+            // lblUyeSoyad
+            // 
+            this.lblUyeSoyad.AutoSize = true;
+            this.lblUyeSoyad.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblUyeSoyad.ForeColor = System.Drawing.Color.White;
+            this.lblUyeSoyad.Location = new System.Drawing.Point(88, 63);
+            this.lblUyeSoyad.Name = "lblUyeSoyad";
+            this.lblUyeSoyad.Size = new System.Drawing.Size(78, 17);
+            this.lblUyeSoyad.TabIndex = 6;
+            this.lblUyeSoyad.Text = "Uye Soyadı";
+            // 
+            // lblUyeAd
+            // 
+            this.lblUyeAd.AutoSize = true;
+            this.lblUyeAd.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblUyeAd.ForeColor = System.Drawing.Color.White;
+            this.lblUyeAd.Location = new System.Drawing.Point(67, 24);
+            this.lblUyeAd.Name = "lblUyeAd";
+            this.lblUyeAd.Size = new System.Drawing.Size(57, 17);
+            this.lblUyeAd.TabIndex = 7;
+            this.lblUyeAd.Text = "Uye Adı";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(83)))), ((int)(((byte)(17)))));
+            this.label8.Location = new System.Drawing.Point(25, 140);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(129, 17);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Son Çalışma Tarihi :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(83)))), ((int)(((byte)(17)))));
+            this.label6.Location = new System.Drawing.Point(25, 102);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(146, 17);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Toplam Çalışılan Saat :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(83)))), ((int)(((byte)(17)))));
+            this.label4.Location = new System.Drawing.Point(25, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Soyadı :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(83)))), ((int)(((byte)(17)))));
+            this.label2.Location = new System.Drawing.Point(25, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Adı :";
+            // 
+            // UserWidgetControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(45)))), ((int)(((byte)(59)))));
+            this.Controls.Add(this.lblSonCalismaTarihi);
+            this.Controls.Add(this.lblToplamCalisilanSaat);
+            this.Controls.Add(this.lblUyeSoyad);
+            this.Controls.Add(this.lblUyeAd);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Name = "UserWidgetControl";
+            this.Size = new System.Drawing.Size(270, 182);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.Label lblSonCalismaTarihi;
+        public System.Windows.Forms.Label lblToplamCalisilanSaat;
+        public System.Windows.Forms.Label lblUyeSoyad;
+        public System.Windows.Forms.Label lblUyeAd;
+    }
+}
--- StudyCheck.FormsUI/UserForms/frmUserPanel.Designer.cs
+++ StudyCheck.FormsUI/UserForms/frmUserPanel.Designer.cs
@@ -28,20 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pcbMinimize = new System.Windows.Forms.PictureBox();
+            this.pcbExit = new System.Windows.Forms.PictureBox();
+            this.pcbSignout = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbMinimize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbExit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbSignout)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(45)))), ((int)(((byte)(59)))));
+            this.panel1.Controls.Add(this.pcbMinimize);
+            this.panel1.Controls.Add(this.pcbExit);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(990, 42);
+            this.panel1.TabIndex = 0;
+            // 
+            // pcbMinimize
+            // 
+            this.pcbMinimize.Image = global::StudyCheck.FormsUI.Properties.Resources.minimize_window_16px;
+            this.pcbMinimize.Location = new System.Drawing.Point(935, 13);
+            this.pcbMinimize.Name = "pcbMinimize";
+            this.pcbMinimize.Size = new System.Drawing.Size(16, 16);
+            this.pcbMinimize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pcbMinimize.TabIndex = 1;
+            this.pcbMinimize.TabStop = false;
+            // 
+            // pcbExit
+            // 
+            this.pcbExit.Image = global::StudyCheck.FormsUI.Properties.Resources.icons8_shutdown_16;
+            this.pcbExit.Location = new System.Drawing.Point(962, 13);
+            this.pcbExit.Name = "pcbExit";
+            this.pcbExit.Size = new System.Drawing.Size(16, 16);
+            this.pcbExit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pcbExit.TabIndex = 1;
+            this.pcbExit.TabStop = false;
+            // 
+            // pcbSignout
+            // 
+            this.pcbSignout.Image = global::StudyCheck.FormsUI.Properties.Resources.exit_sign_32px;
+            this.pcbSignout.Location = new System.Drawing.Point(16, 466);
+            this.pcbSignout.Name = "pcbSignout";
+            this.pcbSignout.Size = new System.Drawing.Size(32, 32);
+            this.pcbSignout.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pcbSignout.TabIndex = 1;
+            this.pcbSignout.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(54, 472);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Çıkış Yap";
             // 
             // frmUserPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(56)))), ((int)(((byte)(74)))));
+            this.ClientSize = new System.Drawing.Size(990, 510);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pcbSignout);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmUserPanel";
             this.Text = "frmUserPanel";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbMinimize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbExit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbSignout)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pcbExit;
+        private System.Windows.Forms.PictureBox pcbMinimize;
+        private System.Windows.Forms.PictureBox pcbSignout;
+        private System.Windows.Forms.Label label1;
     }
 }
