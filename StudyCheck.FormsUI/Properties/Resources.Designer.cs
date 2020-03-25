71d86055d48f01e001d181634a1fd6988765afd8
Adnan Özdemir <xsabrixsabrix@hotmail.com>
2020-03-25 15:49:20 +0300

    UserDashboardControl'e butonlar eklendi

--- StudyCheck.FormsUI/Properties/Resources.Designer.cs
+++ StudyCheck.FormsUI/Properties/Resources.Designer.cs
@@ -263,6 +263,16 @@ namespace StudyCheck.FormsUI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap resume_button_16px {
+            get {
+                object obj = ResourceManager.GetObject("resume_button_16px", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap school_80px {
             get {
                 object obj = ResourceManager.GetObject("school_80px", resourceCulture);
--- StudyCheck.FormsUI/UserForms/UserControls/UserDashboardControl.Designer.cs
+++ StudyCheck.FormsUI/UserForms/UserControls/UserDashboardControl.Designer.cs
@@ -31,6 +31,9 @@
             this.userWidgetBilgiler = new StudyCheck.FormsUI.UserForms.UserControls.UserWidgetControl();
             this.userWidgetSonSinav = new StudyCheck.FormsUI.UserForms.UserControls.ExamControl.ExamInfoControl();
             this.userWidgetSonDers = new StudyCheck.FormsUI.UserForms.UserControls.LessonControl.LessonInfoControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnStartNew = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // userWidgetBilgiler
@@ -57,11 +60,63 @@
             this.userWidgetSonDers.Size = new System.Drawing.Size(278, 92);
             this.userWidgetSonDers.TabIndex = 2;
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(168)))), ((int)(((byte)(204)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(240, 342);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 45);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Son Sınavdan Devam Et";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(191)))), ((int)(((byte)(115)))));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Image = global::StudyCheck.FormsUI.Properties.Resources.resume_button_16px;
+            this.button2.Location = new System.Drawing.Point(435, 342);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(187, 45);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Son Dersten Devam Et";
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // btnStartNew
+            // 
+            this.btnStartNew.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(83)))), ((int)(((byte)(17)))));
+            this.btnStartNew.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStartNew.FlatAppearance.BorderSize = 0;
+            this.btnStartNew.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStartNew.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStartNew.ForeColor = System.Drawing.Color.White;
+            this.btnStartNew.Image = global::StudyCheck.FormsUI.Properties.Resources.add_16px;
+            this.btnStartNew.Location = new System.Drawing.Point(31, 342);
+            this.btnStartNew.Name = "btnStartNew";
+            this.btnStartNew.Size = new System.Drawing.Size(192, 45);
+            this.btnStartNew.TabIndex = 3;
+            this.btnStartNew.Text = "Yeni Çalışma Başlat";
+            this.btnStartNew.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnStartNew.UseVisualStyleBackColor = false;
+            // 
             // UserDashboardControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(56)))), ((int)(((byte)(74)))));
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnStartNew);
             this.Controls.Add(this.userWidgetSonDers);
             this.Controls.Add(this.userWidgetSonSinav);
             this.Controls.Add(this.userWidgetBilgiler);
@@ -76,5 +131,8 @@
         private UserWidgetControl userWidgetBilgiler;
         private ExamControl.ExamInfoControl userWidgetSonSinav;
         private LessonControl.LessonInfoControl userWidgetSonDers;
+        private System.Windows.Forms.Button btnStartNew;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
