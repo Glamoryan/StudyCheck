e0b498c643784622ad9258937168326bf3f2a3f0
Adnan Özdemir <xsabrixsabrix@hotmail.com>
2020-03-25 23:20:23 +0300

    StudyPanel eklendi

---
+++ StudyCheck.FormsUI/UserForms/UserControls/StudyControl/StudyPanel.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace StudyCheck.FormsUI.UserForms.UserControls.StudyControl
+{
+    partial class StudyPanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(21, 47);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(257, 142);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(21, 207);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(257, 142);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(317, 47);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 302);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(556, 47);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(200, 302);
+            this.panel4.TabIndex = 1;
+            // 
+            // StudyPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(56)))), ((int)(((byte)(74)))));
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "StudyPanel";
+            this.Size = new System.Drawing.Size(980, 416);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
