727123283b250e5d1657319dae8c7a5fcc6852fc
Adnan Özdemir <xsabrixsabrix@hotmail.com>
2020-03-25 23:30:38 +0300

    examInfoControl eklendi

---
+++ StudyCheck.FormsUI/UserForms/UserControls/StudyControl/examInfoControl.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace StudyCheck.FormsUI.UserForms.UserControls.StudyControl
+{
+    partial class examInfoControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSinavAdi = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCalisilanToplam = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblCalismaDurum = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblKalanGun = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblSinavAdi
+            // 
+            this.lblSinavAdi.AutoSize = true;
+            this.lblSinavAdi.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSinavAdi.ForeColor = System.Drawing.Color.White;
+            this.lblSinavAdi.Location = new System.Drawing.Point(91, 20);
+            this.lblSinavAdi.Name = "lblSinavAdi";
+            this.lblSinavAdi.Size = new System.Drawing.Size(13, 17);
+            this.lblSinavAdi.TabIndex = 23;
+            this.lblSinavAdi.Text = "-";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(83)))), ((int)(((byte)(17)))));
+            this.label2.Location = new System.Drawing.Point(12, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Sınav Adı :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(83)))), ((int)(((byte)(17)))));
+            this.label1.Location = new System.Drawing.Point(12, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 17);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Sınava Çalışılan Toplam Zaman :";
+            // 
+            // lblCalisilanToplam
+            // 
+            this.lblCalisilanToplam.AutoSize = true;
+            this.lblCalisilanToplam.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCalisilanToplam.ForeColor = System.Drawing.Color.White;
+            this.lblCalisilanToplam.Location = new System.Drawing.Point(224, 54);
+            this.lblCalisilanToplam.Name = "lblCalisilanToplam";
+            this.lblCalisilanToplam.Size = new System.Drawing.Size(64, 17);
+            this.lblCalisilanToplam.TabIndex = 23;
+            this.lblCalisilanToplam.Text = "00:00:00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(83)))), ((int)(((byte)(17)))));
+            this.label4.Location = new System.Drawing.Point(12, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 17);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Çalışma Durumu :";
+            // 
+            // lblCalismaDurum
+            // 
+            this.lblCalismaDurum.AutoSize = true;
+            this.lblCalismaDurum.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCalismaDurum.ForeColor = System.Drawing.Color.White;
+            this.lblCalismaDurum.Location = new System.Drawing.Point(136, 90);
+            this.lblCalismaDurum.Name = "lblCalismaDurum";
+            this.lblCalismaDurum.Size = new System.Drawing.Size(13, 17);
+            this.lblCalismaDurum.TabIndex = 23;
+            this.lblCalismaDurum.Text = "-";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(83)))), ((int)(((byte)(17)))));
+            this.label6.Location = new System.Drawing.Point(12, 126);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(122, 17);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Sınava Kalan Gün :";
+            // 
+            // lblKalanGun
+            // 
+            this.lblKalanGun.AutoSize = true;
+            this.lblKalanGun.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKalanGun.ForeColor = System.Drawing.Color.White;
+            this.lblKalanGun.Location = new System.Drawing.Point(140, 126);
+            this.lblKalanGun.Name = "lblKalanGun";
+            this.lblKalanGun.Size = new System.Drawing.Size(16, 17);
+            this.lblKalanGun.TabIndex = 23;
+            this.lblKalanGun.Text = "0";
+            // 
+            // examInfoControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(45)))), ((int)(((byte)(59)))));
+            this.Controls.Add(this.lblCalismaDurum);
+            this.Controls.Add(this.lblKalanGun);
+            this.Controls.Add(this.lblCalisilanToplam);
+            this.Controls.Add(this.lblSinavAdi);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Name = "examInfoControl";
+            this.Size = new System.Drawing.Size(295, 162);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label lblSinavAdi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Label lblCalisilanToplam;
+        private System.Windows.Forms.Label label4;
+        public System.Windows.Forms.Label lblCalismaDurum;
+        private System.Windows.Forms.Label label6;
+        public System.Windows.Forms.Label lblKalanGun;
+    }
+}
