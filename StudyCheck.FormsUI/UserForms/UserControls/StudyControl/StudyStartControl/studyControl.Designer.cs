c0b8de45e1f35204a93713aaded0bc746694ea71
Adnan Özdemir <xsabrixsabrix@hotmail.com>
2020-03-31 21:33:34 +0300

    studyControl eklendi

---
+++ StudyCheck.FormsUI/UserForms/UserControls/StudyControl/StudyStartControl/studyControl.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace StudyCheck.FormsUI.UserForms.UserControls.StudyControl.StudyStartControl
+{
+    partial class studyControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // studyControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(56)))), ((int)(((byte)(74)))));
+            this.Name = "studyControl";
+            this.Size = new System.Drawing.Size(980, 416);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ StudyCheck.FormsUI/UserForms/UserControls/StudyControl/StudyStartControl/studyControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudyCheck.FormsUI.UserForms.UserControls.StudyControl.StudyStartControl
+{
+    public partial class studyControl : UserControl
+    {
+        public studyControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
