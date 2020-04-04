d6025f415b2862b623790c51070ce5c53ce9e420
Adnan Özdemir <xsabrixsabrix@hotmail.com>
2020-04-04 21:52:12 +0300

    InsufficientTimeException adlı custom exception eklendi

---
+++ StudyCheck.FormsUI/ExceptionManage/CustomExceptions/InsufficientTimeException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyCheck.FormsUI.ExceptionManage.CustomExceptions
+{
+    class InsufficientTimeException:Exception
+    {
+        public InsufficientTimeException():base()
+        {}
+        public InsufficientTimeException(String Mesaj) : base(Mesaj)
+        {
+
+        }
+        public InsufficientTimeException(String Mesaj, Exception innerException) : base(Mesaj, innerException)
+        {
+
+        }
+        public InsufficientTimeException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+
+        }
+    }
+}
